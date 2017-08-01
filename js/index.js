c85772ae19a63c7dd0513a009bdae62476d5a764
Olena Maherovska <omaherovska@n-ix.com>
2017-08-01 18:13:20 +0300

    added new styles

---
+++ js/index.js
@@ -0,0 +1,12 @@
+// $(document).ready(function () {
+// var websites = 10;
+// var counter = 1;
+// var id = setInterval(function(){
+//   $('.counter').text(counter);
+//   counter++;
+//   if(counter > websites){
+//     clearInterval(id);
+//   }
+// }, 50);
+//
+// });
